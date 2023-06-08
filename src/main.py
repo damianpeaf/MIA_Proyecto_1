fe2de9e22711167148f2c0be3abe5140c46d4000
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-06-08 11:52:38 -0600

    fix: validate _ char for params

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ from analyzer.parser import parser
 def main():
     while True:
         try:
-            s = 'rEnAme -pAth->/carpeta1/prueba1.txt -name->b1.txt'
-            # s = 'Configure -type->local -encrypt_log->false -encrypt_read->false'
+            # s = 'rEnAme -pAth->/carpeta1/prueba1.txt -name->b1.txt'
+            s = 'Configure -type->local -encrypt_log->false -encrypt_read->false'
         except EOFError:
             break
         if not s:
@@ -15,15 +15,17 @@ def main():
 
 
 if __name__ == '__main__':
-    # param_name, params = main()
+    param_name, params = main()
+    print(param_name)
+    print(params)
 
     proxy = CommandProxy()
-    result = proxy.execute('configure', {
-        'type': 'local',
-        'encrypt_log': 'false',
-        'encrypt_read': 'false',
-    })
+    # result = proxy.execute('configure', {
+    #     'type': 'local',
+    #     'encrypt_log': 'false',
+    #     'encrypt_read': 'false',
+    # })
 
-    print(Logger.log_messages)
+    # print(Logger.log_messages)
 
-    print(CommandProxy.command_config)
+    # print(CommandProxy.command_config)
