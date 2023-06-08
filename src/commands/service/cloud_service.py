79ea5b910dcd149c80486d53516de9b94484386a
Damian <damianpeaf@gmail.com>
2023-06-08 14:50:38 -0600

    feat: :monocle_face: drive service added

--- src/commands/service/cloud_service.py
+++ src/commands/service/cloud_service.py
@@ -1,11 +1,27 @@
+from __future__ import print_function
+
+import os.path
+
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ['https://www.googleapis.com/auth/drive']
+TOKEN_PATH = os.path.join(os.path.dirname(__file__), 'token.json')
 
 # ? Create an adapter for local/cloud services
 
-# TODO: Create a cloud service
+
 class CloudFileService:
 
     def __init__(self) -> None:
-        pass
+        self._drive_service = None
+
+        creds = Credentials.from_authorized_user_file(TOKEN_PATH, SCOPES)
+        try:
+            self._drive_service = build('drive', 'v3', credentials=creds)
+        except HttpError as error:
+            print(f'An error occurred with drive auth: {error}')
 
     def _create_root(self):
         pass
